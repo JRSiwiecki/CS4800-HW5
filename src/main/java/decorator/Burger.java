bc93aa7fa06f0f662854c62de8ce05f259ffe0db
JRSiwiecki <JRSiwiecki@gmail.com>
2024-03-26 19:16:50 -0700

    Add topping list to burger class

--- src/main/java/decorator/Burger.java
+++ src/main/java/decorator/Burger.java
@@ -1,17 +1,26 @@
 package decorator;
 
+import java.util.ArrayList;
+
 public class Burger implements FoodItem
 {
     private final double basePrice;
+    private final ArrayList<String> toppings;
 
     public Burger()
     {
         basePrice = 1.00;
+        toppings = new ArrayList<>();
     }
 
     @Override
     public double calculateCost()
     {
-        return basePrice;
+        return basePrice + (toppings.size() * 0.25);
+    }
+
+    public void addTopping(String topping)
+    {
+        toppings.add(topping);
     }
 }
