ccf762dadec5f435854ece4745c85be7f5ea9b06
JRSiwiecki <JRSiwiecki@gmail.com>
2024-03-26 19:44:21 -0700

    Add CheeseBurgerTest

---
+++ src/test/java/decorator/CheeseBurgerTest.java
@@ -0,0 +1,25 @@
+package decorator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CheeseBurgerTest
+{
+
+    @Test
+    void calculateCost()
+    {
+        CheeseBurger cheeseBurger = new CheeseBurger(new Burger());
+        assertEquals(2.00, cheeseBurger.calculateCost());
+    }
+
+    @Test
+    void addTopping()
+    {
+        CheeseBurger cheeseBurger = new CheeseBurger(new Burger());
+        cheeseBurger.addTopping("extra salt");
+        cheeseBurger.addTopping("extra cheese");
+        assertEquals(2.50, cheeseBurger.calculateCost());
+    }
+}
